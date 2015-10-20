46c224b14b2e01b4a7c050af31a7aa8550e97c0b
aidenbenner <aiden.benner@gmail.com>
2015-10-19 23:07:01 -0600

    Added more deadzones

--- MecDrive.c
+++ MecDrive.c
@@ -16,17 +16,27 @@ void initMec( DriveBase* db){
 	mec = *db;
 }
 
-
 void mecDrive(){
-	if(abs(x) > JOYSTICK_DEADZONE){
-		float x = vexRT[Ch4];
+	float x;
+	float y;
+	float speed;
+	float turnSpeed;
+	if(abs(vexRT[Ch4]) > JOYSTICK_DEADZONE){
+		x = vexRT[Ch4];
 	}
-	if(abs(y) > JOYSTICK_DEADZONE){
-		float y = vexRT[Ch3];
+	if(abs(vexRT[Ch3]) > JOYSTICK_DEADZONE){
+		y = vexRT[Ch3];
+		
 	}
+	if(abs(vexRT[Ch2]) > JOYSTICK_DEADZONE){
+	 	speed = vexRT[Ch2]/127;
+	}
+	if(abs(vexRT[Ch2]) > JOYSTICK_DEADZONE){
+		turnSpeed = vexRT[Ch1]/127;
+	}
+
 	float heading = atan(y/x);
-	float speed = vexRT[Ch2]/127;
-	float turnSpeed = vexRT[Ch3]/127;
+
 	*mec.fl = 127 * (speed * sin(heading + (PI/4)) + turnSpeed;
 	*mec.fr = 127 * (speed * cos(heading + (PI/4)) - turnSpeed;
 	*mec.bl = 127 * (speed * cos(heading + (PI/4)) + turnSpeed;
